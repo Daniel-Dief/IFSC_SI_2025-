78747edfdd47eb7856e58db2f43ceef1475d2f66
Daniel Santos <danieldiefsantos@gmail.com>
2025-04-01 21:24:39 -0300

    feat: atividades do dia

--- EDD/04/01_04/1.py
+++ EDD/04/01_04/1.py
@@ -6,11 +6,24 @@ class Pilha:
         self.ultimaPosicao = -1
         self.valores = np.empty(self.capacidade, dtype=int)
 
+    def pilhaCheia(self):
+        if(self.ultimaPosicao == self.capacidade -1):
+            return True
+        else:
+            return False
+        
+    def pilhaVazia(self):
+        if(self.ultimaPosicao == -1):
+            return True
+        else:
+            return False
+
     def verTopo(self):
         if(self.ultimaPosicao == -1):
             print("A pilha está vazia!")
             return -1
             
+        print(self.valores[self.ultimaPosicao])
         return self.valores[self.ultimaPosicao]
     
     def empilhar(self, valor):
@@ -29,3 +42,15 @@ class Pilha:
         self.ultimaPosicao -= 1
         return self.valores[self.ultimaPosicao + 1]
     
+x = Pilha(10)
+x.empilhar(5)
+x.empilhar(3)
+x.empilhar(2)
+x.empilhar(5)
+x.empilhar(9)
+x.empilhar(45)
+x.verTopo()
+x.desenpilhar()
+x.desenpilhar()
+x.desenpilhar()
+x.verTopo()
